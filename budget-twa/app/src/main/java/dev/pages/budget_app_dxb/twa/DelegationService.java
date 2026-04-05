affc8727024c1a981b6033cbea2401a44766ff27
 Viacheslav <viacheslav@Viacheslavs-MacBook-Pro.local>
2026-04-05 19:51:46 +0300

    Added apk build

---
+++ budget-twa/app/src/main/java/dev/pages/budget_app_dxb/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package dev.pages.budget_app_dxb.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
